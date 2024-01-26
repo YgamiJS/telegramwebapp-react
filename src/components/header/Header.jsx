97759a6b9ece6221ae802a05d607c795194467e6
Dima <dimasakharov@icloud.com>
2024-01-26 18:40:26 +0300

    second

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,17 +1,13 @@
 import styled from "./Header.css";
 import { useTelegram } from "../../hooks/useTelegram";
 
-const tg = window.Telegram.WebApp;
-
 const Header = () => {
   const { user, onClose } = useTelegram();
 
   return (
     <div>
       <button onClick={onClose}>закрыть</button>
-      <span className={styled.username}>
-        {tg.initDataUnsafe?.user?.username}
-      </span>
+      <span className={styled.username}>{user?.username}</span>
     </div>
   );
 };
